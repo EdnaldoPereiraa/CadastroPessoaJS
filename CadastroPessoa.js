ecac5412714514a1c8e529cf101e072804f4cfa4
Ednaldo Pereira <141043267+EdnaldoPereiraa@users.noreply.github.com>
2023-11-29 10:41:12 -0300

    Create CadastroPessoa.js
    Praticando Javascript, curso FrontEnd

---
+++ CadastroPessoa.js
@@ -0,0 +1,14 @@
+// Nome: String
+// Salário: Number
+// Idade: Number
+// Possui diploma: Boolean
+
+let nome = "Ednaldo";
+let salario = 2500;
+let idade = 26;
+let possuiDiploma = true;
+
+console.log("Nome:", nome);
+console.log("Salário:", salario);
+console.log("Idade:", idade);
+console.log("Possui Diploma:", possuiDiploma);
